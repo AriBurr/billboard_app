3d41e2805570a7c4ade8a47530e88b065ff16672
Ariel Burriss <ariburr@gmail.com>
2017-11-30 23:49:07 -0700

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
-25.times do |n|
+Billboard.create(name: 'Bossa Nova Classics')
+
+25.times do
   name = Faker::BossaNova.artist
-Artist.create!(name: name,
-               genre: 'BossaNova')
+  Artist.create(name: name,
+                 genre: 'Bossa Nova')
 end
 
-25.times do |n|
+25.times do
   title = Faker::BossaNova.song
-Song.create!(title: title,
-             artist_id: rand(1..25),
-             billboard_id: nil)
+  Song.create(title: title,
+               artist_id: rand(1..25),
+               billboard_id: nil)
 end
